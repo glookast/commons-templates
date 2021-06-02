43e58a9340a5c30bf15fefa5b047310faa48ea49
Tiago Pereira <tiagoafpereira@gmail.com>
2021-06-02 15:34:32 +0100

    Fix missing 'assertEquals' import
    Other changes:
    
      - Updated version to 3.3.0-RC7-SNAPSHOT

--- src/test/java/com/glookast/commons/templates/EditShareFlowMAMSystemTest.java
+++ src/test/java/com/glookast/commons/templates/EditShareFlowMAMSystemTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class EditShareFlowStorageSystemTest {
